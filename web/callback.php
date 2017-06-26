131dcbc81d4c92183d658d79028db37842bed45c
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-06-26 16:26:50 +0900

    k

--- web/callback.php
+++ web/callback.php
@@ -326,7 +326,7 @@ $sql = "INSERT INTO botlog (userid, contents, return) VALUES ('$userID', '$text'
 $result_flag = pg_query($sql);
 
 
-$result = pg_query('SELECT *  FROM botlog');
+$result = pg_query('SELECT userid  FROM botlog');
 if ($result==null) {
 	error_log(リザルトに入っていません);
 }else{
