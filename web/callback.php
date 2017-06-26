984c7c04359b4d6f3c93b2fc4ca024d7769694b9
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-06-26 15:54:30 +0900

    l

--- web/callback.php
+++ web/callback.php
@@ -326,7 +326,7 @@ $sql = "INSERT INTO botlog (userid, contents, return) VALUES ('$userID', '$text'
 $result_flag = pg_query($sql);
 
 
-$result = pg_query('SELECT * FROM botlog');
+$result = pg_query('SELECT userid FROM botlog');
 error_log($result);
 
 pg_close($conn);
