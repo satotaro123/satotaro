06b50054780545493e9c0fc88a7acc91bc84a416
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-06-27 14:13:55 +0900

    g

--- web/callback.php
+++ web/callback.php
@@ -196,12 +196,27 @@ $userArray [$userID] ["cid"] = $conversation_id;
 $userArray [$userID] ["time"] = date ( 'Y/m/d H:i:s' );
 //$lastConversationData [];
 
+//データベースへの接続
+$conn = "host=ec2-54-83-26-65.compute-1.amazonaws.com dbname=daj2h828dej8bv user=hjxiibzzbialkm
+ password=227ba653a1200a8a8bf40645763da904bfca62e1ee9e64b6f68ca2f7824da99d";
+$link = pg_connect($conn);
+if (!$link) {
+	error_log(接続に失敗);
+}else{
+	error_log(接続に成功);
+}
+
+$node = pg_query('SELECT dnode FROM cvsdata');
+
+//データベースの切断
+pg_close($conn);
+
 $data ["context"] = array (
 		"conversation_id" => $conversation_id,
 		"system" => array (
 				"dialog_stack" => array (
 						array (
-								"dialog_node" => "root"
+								"dialog_node" => "$node"
 						)
 				),
 				"dialog_turn_counter" => 1,
