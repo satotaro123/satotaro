e39dd9e337d06b438fdb0b094bf32781524e198e
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-07-03 11:10:59 +0900

    k

--- web/callback.php
+++ web/callback.php
@@ -407,7 +407,7 @@ if (! $link) {
 
 // cvsdataテーブルでデータ変更
 
-$result = pg_query('SELECT userid, conversationid, dnode FROM cvsdata WHERE dnode = 粗大ごみの出し方');
+$result = pg_query('SELECT userid, conversationid, dnode FROM cvsdata ');
 $rows = pg_fetch_array($result, NULL, PGSQL_ASSOC);
 error_log('413');
 error_log($rows[conversationid]);
