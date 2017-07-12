44d216aef0687542323562cafd1d20406e254aed
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-07-12 13:10:45 +0900

    k

--- web/callback.php
+++ web/callback.php
@@ -154,7 +154,7 @@ if ($type != "text") {
 	curl_setopt ( $curl, CURLOPT_URL, $url );
 	// curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: multipart/form-data'));
 	curl_setopt ( $curl, CURLOPT_POST, TRUE );
-	curl_setopt ( $curl, CURLOPT_POSTFIELDS, "images_file=$imagedata");
+	curl_setopt ( $curl, CURLOPT_POSTFIELDS, "images_file=@$imagedata");
 	curl_setopt ( $curl, CURLOPT_VERBOSE, TRUE );
 	curl_setopt ( $curl, CURLOPT_RETURNTRANSFER, TRUE );
 
