cb659fa509b049e7e77d6029678c2bb5b6038d64
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-07-14 11:56:12 +0900

    a

--- web/callback.php
+++ web/callback.php
@@ -164,7 +164,8 @@ if ($type != "text") {
 	$result = curl_exec ( $ch );
 	curl_close($ch);
 
-	$image_resource = imagecreatefromstring($result);
+	$zyuurokusinnsu = bin2hex($result);
+	error_log ($zyuurokusinnsu);
 
 	error_log ( 181 );
 	error_log ( $json_string);
