5c524fb1b88791e6552710bf2b70e8d194f9c6ac
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-07-18 10:29:54 +0900

    k

--- web/callback.php
+++ web/callback.php
@@ -158,14 +158,14 @@ if ($type != "text") {
 	$ch = curl_init ("https://api.line.me/v2/bot/message/".$messageId."/content") ;
 	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
 	curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
-			'Content-Type: application/json; charser=UTF-8',
+			'Content-Type: image/jpeg; charser=UTF-8',
 			'Authorization: Bearer ' . $accessToken
 	) );
 	$result = curl_exec ( $ch );
 	curl_close($ch);
 
 	$data = file_get_contents($result);
-	header('Content-type: image/jpeg');
+
 
 
 
